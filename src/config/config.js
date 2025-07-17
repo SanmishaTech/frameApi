45d345918616318c9e20f8994ce8293a1531041a
Sanjeev Divekar <sanjeev@sanmisha.com>
2025-07-17 16:35:05 +0530

    Fix config.js: update frontend URL to correct domain

--- src/config/config.js
+++ src/config/config.js
@@ -2,5 +2,5 @@ module.exports = {
   appName: process.env.APP_NAME || "Frame",
   defaultUserRole: process.env.DEFAULT_USER_ROLE || "user",
   allowRegistration: process.env.ALLOW_REGISTRATION ?? false,
-  frontendUrl: process.env.FRONTEND_URL || "https://frame.saaaws.co.in",
+  frontendUrl: process.env.FRONTEND_URL || "https://frame.sanmishaaws.co.in",
 };
