17289cff80094d3e1d909f3295421602090bdc1f
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2025-08-22 15:30:48 +0530

    a

--- src/middleware/VideoUploadMiddleware.js
+++ src/middleware/VideoUploadMiddleware.js
@@ -1,55 +1,3 @@
-// const fs = require("fs");
-// const path = require("path");
-// const multer = require("multer");
-
-// const allowedMimeTypes = [
-//   "video/webm",
-//   "video/mp4",
-//   "video/ogg",
-//   "video/x-matroska",
-//   "video/quicktime",
-//   "video/avi",
-// ];
-
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     const uuid = req.params.uuid;
-
-//     var uploadPath = path.join(__dirname, "../../uploads", uuid, "temp");
-//     if (process.env.IS_PRODUCTION === "true") {
-//       uploadPath = `/uploads/${uuid}/temp`;
-//     }
-//     // Target folder: /uploads/<uuid>
-
-//     // Ensure the directory exists
-//     fs.mkdirSync(uploadPath, { recursive: true });
-
-//     cb(null, uploadPath);
-//   },
-//   filename: function (req, file, cb) {
-//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-//     cb(
-//       null,
-//       req.params.uuid + "-" + uniqueSuffix + path.extname(file.originalname)
-//     );
-//   },
-// });
-
-// const fileFilter = (req, file, cb) => {
-//   if (allowedMimeTypes.includes(file.mimetype)) {
-//     cb(null, true);
-//   } else {
-//     cb(new Error("Unsupported file type. Only video files are allowed."));
-//   }
-// };
-
-// const upload = multer({
-//   storage,
-//   limits: { fileSize: 50 * 1024 * 1024 }, // 50MB
-//   fileFilter,
-// });
-
-// module.exports = upload;
 const fs = require("fs");
 const path = require("path");
 const multer = require("multer");
@@ -81,8 +29,12 @@ const storage = multer.diskStorage({
   filename: function (req, file, cb) {
     // Use filename from req.body.filename if provided, otherwise generate unique
     let filename = req.body.filename;
+    // console.log("Filename is :", filename);
+    // console.log(`Filename is : ${filename}`);
 
     if (filename) {
+      // console.log("Right");
+
       // Sanitize filename to avoid path traversal attacks
       filename = path.basename(filename);
 
@@ -91,6 +43,7 @@ const storage = multer.diskStorage({
         filename += path.extname(file.originalname);
       }
     } else {
+      // console.log("Wrong");
       // fallback to original logic if filename not provided
       const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
       filename =
